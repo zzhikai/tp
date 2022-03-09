e0957d21145637deb7f65a4997a858c6dd7d50e0
RyanCheungJF <ryan9cheung@gmail.com>
2022-03-09 12:02:04 +0800

    Refactor paths to fix tests

--- src/main/java/seedu/linkedout/model/UserPrefs.java
+++ src/main/java/seedu/linkedout/model/UserPrefs.java
@@ -14,8 +14,7 @@ import seedu.linkedout.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
-    private Path linkedoutBookFilePath = Paths.get("data" , "linkedout.json");
+    private Path linkedoutFilePath = Paths.get("data" , "linkedout.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
@@ -36,7 +35,7 @@ public class UserPrefs implements ReadOnlyUserPrefs {
     public void resetData(ReadOnlyUserPrefs newUserPrefs) {
         requireNonNull(newUserPrefs);
         setGuiSettings(newUserPrefs.getGuiSettings());
-        setAddressBookFilePath(newUserPrefs.getLinkedoutFilePath());
+        setLinkedoutFilePath(newUserPrefs.getLinkedoutFilePath());
     }
 
     public GuiSettings getGuiSettings() {
@@ -49,18 +48,12 @@ public class UserPrefs implements ReadOnlyUserPrefs {
     }
 
     public Path getLinkedoutFilePath() {
-        // return linkedoutBookFilePath;
-        return addressBookFilePath;
+        return linkedoutFilePath;
     }
 
-    public void setAddressBookFilePath(Path addressBookFilePath) {
-        requireNonNull(addressBookFilePath);
-        this.addressBookFilePath = addressBookFilePath;
-    }
-
-    public void setLinkedoutFilePath(Path linkedoutBookFilePath) {
-        requireNonNull(linkedoutBookFilePath);
-        this.linkedoutBookFilePath = linkedoutBookFilePath;
+    public void setLinkedoutFilePath(Path linkedoutFilePath) {
+        requireNonNull(linkedoutFilePath);
+        this.linkedoutFilePath = linkedoutFilePath;
     }
 
     @Override
@@ -75,19 +68,19 @@ public class UserPrefs implements ReadOnlyUserPrefs {
         UserPrefs o = (UserPrefs) other;
 
         return guiSettings.equals(o.guiSettings)
-                && addressBookFilePath.equals(o.addressBookFilePath);
+                && linkedoutFilePath.equals(o.linkedoutFilePath);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(guiSettings, addressBookFilePath);
+        return Objects.hash(guiSettings, linkedoutFilePath);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Gui Settings : " + guiSettings);
-        sb.append("\nLocal data file location : " + addressBookFilePath);
+        sb.append("\nLocal data file location : " + linkedoutFilePath);
         return sb.toString();
     }
 
