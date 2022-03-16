10fd9c5ec2f3081d5967ee3f0a5f1b8deafe2d40
Ryan Tan <ryantanlien99@gmail.com>
2022-03-16 13:31:30 +0800

    Update JobTest.java
    Added a suite of tests in order to check for invalid non-alphanumeric inputs.

--- src/test/java/seedu/linkedout/model/applicant/JobTest.java
+++ src/test/java/seedu/linkedout/model/applicant/JobTest.java
@@ -1,7 +1,6 @@
 package seedu.linkedout.model.applicant;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.linkedout.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -9,14 +8,74 @@ import org.junit.jupiter.api.Test;
 public class JobTest {
 
     @Test
-    public void constructor_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new Job(null));
+    public void constructor_emptyJob_throwsIllegalArgumentException() {
+        String emptyJob = "";
+        assertThrows(IllegalArgumentException.class, () -> new Job(emptyJob));
     }
 
     @Test
     public void constructor_invalidJob_throwsIllegalArgumentException() {
-        String invalidJob = "";
+        String invalidJob = "#";
         assertThrows(IllegalArgumentException.class, () -> new Job(invalidJob));
+    }
+
+    @Test
+    public void constructor_invalidJobWhitespace_throwsIllegalArgumentException() {
+        String invalidJob = " ";
+        assertThrows(IllegalArgumentException.class, () -> new Job(invalidJob));
+    }
+
+    @Test
+    public void constructor_validJob_valueEqualsParameter() {
+        String validJob = "job1";
+        Job job = new Job(validJob);
+        assertEquals(job.value, "job1");
+    }
+
+    @Test
+    public void isValidJob_whitespace_false() {
+        String invalidJob = " ";
+        assertFalse(Job.isValidJob(invalidJob));
+    }
+
+    @Test
+    public void isValidJob_nonAlphaNumeric_false() {
+        final int ASCII_CHARACTER_EXCLAMATION_MARK = 33;
+        final int ASCII_CHARACTER_BACKSLASH = 47;
+        final int ASCII_CHARACTER_COLON = 58;
+        final int ASCII_CHARACTER_AT = 64;
+        final int ASCII_CHARACTER_SQUARE_BRACKET = 91;
+        final int ASCII_CHARACTER_BACK_TICK = 96;
+        final int ASCII_CHARACTER_LEFT_CURLY_BRACE = 123;
+        final int ASCII_CHARACTER_DEL = 127;
+
+        char character = ASCII_CHARACTER_EXCLAMATION_MARK;
+        while (character <= ASCII_CHARACTER_BACKSLASH) {
+            String jobValue = String.valueOf(character);
+            assertFalse(Job.isValidJob(jobValue));
+            character++;
+        }
+
+        character = ASCII_CHARACTER_COLON;
+        while(character <= ASCII_CHARACTER_AT) {
+            String jobValue = String.valueOf(character);
+            assertFalse(Job.isValidJob(jobValue));
+            character++;
+        }
+
+        character = ASCII_CHARACTER_SQUARE_BRACKET;
+        while (character <= ASCII_CHARACTER_BACK_TICK) {
+            String jobValue = String.valueOf(character);
+            assertFalse(Job.isValidJob(jobValue));
+            character++;
+        }
+
+        character = ASCII_CHARACTER_LEFT_CURLY_BRACE;
+        while (character <= ASCII_CHARACTER_DEL) {
+            String jobValue = String.valueOf(character);
+            assertFalse(Job.isValidJob(jobValue));
+            character++;
+        }
     }
 
     @Test
@@ -30,7 +89,27 @@ public class JobTest {
 
         // valid jobs
         assertTrue(Job.isValidJob("Software Engineer"));
-        //assertTrue(Job.isValidJob("-")); // one character
+        assertTrue(Job.isValidJob("J")); // one character
         assertTrue(Job.isValidJob("Full Stack Software Engineer")); // long job
+    }
+
+    @Test
+    public void equals_thisJob_true() {
+        Job job = new Job("job");
+        assertTrue(job.equals(job));
+    }
+
+    @Test
+    public void equals_otherType_false() {
+        Job job = new Job("job");
+        String string = "job";
+        assertFalse(job.equals(string));
+    }
+
+    @Test
+    public void equals_otherJobSameValue_true() {
+        Job job = new Job("job");
+        Job otherJob = new Job("otherJob");
+        assertFalse(job.equals(otherJob));
     }
 }
