89a7358184b69dfb79d9153b1682fd2044666a58
RyanCheungJF <ryan9cheung@gmail.com>
2022-03-27 12:12:20 +0800

    Add tests

--- src/test/java/seedu/linkedout/logic/commands/SearchCommandTest.java
+++ src/test/java/seedu/linkedout/logic/commands/SearchCommandTest.java
@@ -5,9 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.linkedout.commons.core.Messages.MESSAGE_APPLICANTS_LISTED_OVERVIEW;
 import static seedu.linkedout.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.linkedout.testutil.TypicalApplicants.ALICE;
 import static seedu.linkedout.testutil.TypicalApplicants.CARL;
+import static seedu.linkedout.testutil.TypicalApplicants.DANIEL;
 import static seedu.linkedout.testutil.TypicalApplicants.ELLE;
 import static seedu.linkedout.testutil.TypicalApplicants.FIONA;
+import static seedu.linkedout.testutil.TypicalApplicants.GEORGE;
 import static seedu.linkedout.testutil.TypicalApplicants.getTypicalLinkedout;
 
 import java.util.Arrays;
@@ -18,7 +21,9 @@ import org.junit.jupiter.api.Test;
 import seedu.linkedout.model.Model;
 import seedu.linkedout.model.ModelManager;
 import seedu.linkedout.model.UserPrefs;
+import seedu.linkedout.model.applicant.JobContainsKeywordsPredicate;
 import seedu.linkedout.model.applicant.NameContainsKeywordsPredicate;
+import seedu.linkedout.model.applicant.RoundContainsKeywordsPredicate;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code SearchCommand}.
@@ -55,9 +60,11 @@ public class SearchCommandTest {
     }
 
     @Test
-    public void execute_zeroKeywords_noApplicantFound() {
+    public void executeOnName_zeroKeywords_noApplicantFound() {
         String expectedMessage = String.format(MESSAGE_APPLICANTS_LISTED_OVERVIEW, 0);
-        NameContainsKeywordsPredicate predicate = preparePredicate(" ");
+
+        // test model for empty name
+        NameContainsKeywordsPredicate predicate = prepareNamePredicate(" ");
         SearchCommand command = new SearchCommand(predicate);
         expectedModel.updateFilteredApplicantList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
@@ -65,19 +72,80 @@ public class SearchCommandTest {
     }
 
     @Test
-    public void execute_multipleKeywords_multipleApplicantsFound() {
+    public void executeOnJob_zeroKeywords_noApplicantFound() {
+        String expectedMessage = String.format(MESSAGE_APPLICANTS_LISTED_OVERVIEW, 0);
+
+        // test model for empty job
+        JobContainsKeywordsPredicate predicate = prepareJobPredicate(" ");
+        SearchCommand command = new SearchCommand(predicate);
+        expectedModel.updateFilteredApplicantList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredApplicantList());
+    }
+
+    @Test
+    public void executeOnRound_zeroKeywords_noApplicantFound() {
+        String expectedMessage = String.format(MESSAGE_APPLICANTS_LISTED_OVERVIEW, 0);
+
+        // test model for empty round
+        RoundContainsKeywordsPredicate predicate = prepareRoundPredicate(" ");
+        SearchCommand command = new SearchCommand(predicate);
+        expectedModel.updateFilteredApplicantList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredApplicantList());
+    }
+
+    @Test
+    public void executeOnName_multipleKeywords_multipleApplicantsFound() {
+        // test model for name
         String expectedMessage = String.format(MESSAGE_APPLICANTS_LISTED_OVERVIEW, 3);
-        NameContainsKeywordsPredicate predicate = preparePredicate("Kurz Elle Kunz");
+        NameContainsKeywordsPredicate predicate = prepareNamePredicate("Kurz Elle Kunz");
         SearchCommand command = new SearchCommand(predicate);
         expectedModel.updateFilteredApplicantList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
         assertEquals(Arrays.asList(CARL, ELLE, FIONA), model.getFilteredApplicantList());
     }
 
+    @Test
+    public void executeOnJob_multipleKeywords_multipleApplicantsFound() {
+        // test model for job
+        String expectedMessage = String.format(MESSAGE_APPLICANTS_LISTED_OVERVIEW, 6);
+        JobContainsKeywordsPredicate predicate = prepareJobPredicate("Developer Engineer");
+        SearchCommand command = new SearchCommand(predicate);
+        expectedModel.updateFilteredApplicantList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(ALICE, CARL, DANIEL, ELLE, FIONA, GEORGE), model.getFilteredApplicantList());
+    }
+
+    @Test
+    public void executeOnRound_multipleKeywords_multipleApplicantsFound() {
+        // test model for round
+        String expectedMessage = String.format(MESSAGE_APPLICANTS_LISTED_OVERVIEW, 2);
+        RoundContainsKeywordsPredicate predicate = prepareRoundPredicate("Job Test");
+        SearchCommand command = new SearchCommand(predicate);
+        expectedModel.updateFilteredApplicantList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(ELLE, FIONA), model.getFilteredApplicantList());
+    }
+
     /**
      * Parses {@code userInput} into a {@code NameContainsKeywordsPredicate}.
      */
-    private NameContainsKeywordsPredicate preparePredicate(String userInput) {
+    private NameContainsKeywordsPredicate prepareNamePredicate(String userInput) {
         return new NameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code JobContainsKeywordsPredicate}.
+     */
+    private JobContainsKeywordsPredicate prepareJobPredicate(String userInput) {
+        return new JobContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code RoundContainsKeywordsPredicate}.
+     */
+    private RoundContainsKeywordsPredicate prepareRoundPredicate(String userInput) {
+        return new RoundContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
     }
 }
--- src/test/java/seedu/linkedout/logic/parser/SearchCommandParserTest.java
+++ src/test/java/seedu/linkedout/logic/parser/SearchCommandParserTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import seedu.linkedout.logic.commands.SearchCommand;
 import seedu.linkedout.model.applicant.JobContainsKeywordsPredicate;
 import seedu.linkedout.model.applicant.NameContainsKeywordsPredicate;
+import seedu.linkedout.model.applicant.RoundContainsKeywordsPredicate;
 
 public class SearchCommandParserTest {
 
@@ -23,12 +24,16 @@ public class SearchCommandParserTest {
 
     @Test
     public void parse_emptyPrefix_throwsParseException() {
-        //missing name prefix
+        // missing name prefix
         assertParseFailure(parser, "Alice Bob", String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_USAGE));
 
-        //missing job prefix
+        // missing job prefix
         assertParseFailure(parser, "Engineer", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_USAGE));
+
+        // missing round prefix
+        assertParseFailure(parser, "Technical Interview", String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_USAGE));
     }
 
@@ -41,6 +46,10 @@ public class SearchCommandParserTest {
         // missing job keyword
         assertParseFailure(parser, " j/", String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_CONSTRAINTS));
+
+        // missing round keyword
+        assertParseFailure(parser, " r/", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_CONSTRAINTS));
     }
 
     @Test
@@ -49,22 +58,28 @@ public class SearchCommandParserTest {
                 new SearchCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
         SearchCommand expectedJobSearchCommand =
                 new SearchCommand(new JobContainsKeywordsPredicate(Arrays.asList("Software", "Engineer")));
+        SearchCommand expectedRoundSearchCommand =
+                new SearchCommand(new RoundContainsKeywordsPredicate(Arrays.asList("Technical", "Interview")));
 
         // no leading and trailing whitespaces
         assertParseSuccess(parser, " n/Alice Bob", expectedNameSearchCommand);
         assertParseSuccess(parser, " j/Software Engineer", expectedJobSearchCommand);
+        assertParseSuccess(parser, " r/Technical Interview", expectedRoundSearchCommand);
 
         // multiple whitespaces between keywords
         assertParseSuccess(parser, " n/Alice \n \t Bob  \t", expectedNameSearchCommand);
         assertParseSuccess(parser, " j/Software \n \t Engineer  \t", expectedJobSearchCommand);
+        assertParseSuccess(parser, " r/Technical \n \t Interview            \t", expectedRoundSearchCommand);
 
         //leading whitespace after prefix
         assertParseSuccess(parser, " n/\nAlice \n \t Bob  \t", expectedNameSearchCommand);
         assertParseSuccess(parser, " j/                  Software \n \t Engineer  \t", expectedJobSearchCommand);
+        assertParseSuccess(parser, " r/        Technical     \n         Interview  ", expectedRoundSearchCommand);
 
         //case insensitive
         assertParseSuccess(parser, " n/AlIce BOb", expectedNameSearchCommand);
         assertParseSuccess(parser, " j/soFTwaRe enGiNeer", expectedJobSearchCommand);
+        assertParseSuccess(parser, " r/teChnIcaL inTerVieW", expectedRoundSearchCommand);
 
     }
 
